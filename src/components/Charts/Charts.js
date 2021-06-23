3cffe578d2d7116b65b28af9ef26c0c55dbfe708
Ara Hovhannisyan <ara90hovhannisyan@gmail.com>
2021-06-23 18:22:53 +0400

    Format temperature bar tooltip

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -1,13 +1,16 @@
-import { BarChart, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { BarChart, XAxis, YAxis, Tooltip, Legend, Bar, CartesianGrid } from 'recharts';
 
 export const Charts = ({ data }) => {
   return (
     <>
       <h2 style={{ textAlign: 'center' }}>Temperature Data</h2>
       <BarChart layout="vertical" width={1200} height={800} data={data.list}>
+        <CartesianGrid strokeDasharray="1 1" />
         <XAxis type="number" />
         <YAxis type="category" dataKey="dt_txt" />
-        <Tooltip />
+        <Tooltip formatter={(value) => {
+          return Math.round(value);
+        }} />
         <Legend />
         <Bar name="Temperature" dataKey="main.temp" fill="#9acd32" />
       </BarChart>
